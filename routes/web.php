2dea4037277d291b12ebda0405b4e30bad2450f5
dishant1996 <dishantyashwante1996@gmail.com>
2023-07-23 14:14:16 -0700

    added get method

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DemoController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/',[DemoController::class,'welcomew'])->name('home'); 
+
+
+// Route::get('/{new}',[DemoController::class,'welcomew'])->name('home'); 
+//new string is passed which is also needed to pass in controller function method
+
+Route::get('/user/{id}/{city}',[DemoController::class,'userw'])->name('user');  
+ //userw is method created in controller
+
+Route::get('/blog',[DemoController::class,'showblog'])->name('blog'); //anchor
